af4534bcafb8448c8123af1e721e461049a8cccb
bidep <lequocanh270502@gmail.com>
2024-12-23 21:42:22 +0700

    test

--- Beematching/Controllers/HomeController.cs
+++ Beematching/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Beematching.Models;
+using Beematching.Model;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -23,10 +23,6 @@ namespace Beematching.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
